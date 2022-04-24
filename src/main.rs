2cea1f4c0d198d7c2b9942eb6e4b71a1b8a88695
Teddy Byron <ted@tedbyron.com>
2022-04-24 18:09:46 -0400

    2d shapes

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,11 @@
-#![warn(clippy::all, clippy::nursery, clippy::pedantic, rust_2018_idioms)]
+#![warn(clippy::all, clippy::nursery, rust_2018_idioms)]
 #![allow(clippy::needless_pass_by_value)]
 #![doc = include_str!("../README.md")]
 
-use nannou::prelude::*;
+mod shapes;
+
+use shapes::*;
 
 fn main() {
     nannou::app(model).update(update).run();
-}
-
-struct Model {
-    _window: window::Id,
-}
-
-fn model(app: &App) -> Model {
-    let w = app.new_window().view(view).build().unwrap();
-    Model { _window: w }
-}
-
-fn update(_app: &App, _model: &mut Model, _update: Update) {}
-
-fn view(app: &App, _model: &Model, frame: Frame<'_>) {
-    let draw = app.draw();
-    let win = app.window_rect();
-
-    draw.background().color(SLATEBLUE);
-    draw.ellipse().color(STEELBLUE);
-
-    let win_p = win.pad(25.0);
-    let square = Rect::from_w_h(100.0, 100.0).top_left_of(win_p);
-
-    draw.rect().xy(square.xy()).wh(square.wh()).color(PLUM);
-
-    let circle = square.below(square);
-    draw.ellipse().xy(circle.xy()).wh(circle.wh()).color(SALMON);
-
-    draw.to_frame(app, &frame).unwrap();
 }
---
+++ src/shapes.rs
@@ -0,0 +1,46 @@
+use nannou::prelude::*;
+
+pub struct Model {
+    #[allow(dead_code)]
+    window: window::Id,
+}
+
+pub fn model(app: &App) -> Model {
+    let window = app.new_window().view(view).build().unwrap();
+    Model { window }
+}
+
+pub fn update(_app: &App, _model: &mut Model, _update: Update) {}
+
+pub fn view(app: &App, _model: &Model, frame: Frame<'_>) {
+    let draw = app.draw();
+    let win = app.window_rect();
+
+    draw.background().color(SLATEBLUE);
+
+    let win_p = win.pad(25.0);
+    let square = Rect::from_w_h(100.0, 100.0).top_left_of(win_p);
+
+    draw.rect().xy(square.xy()).wh(square.wh()).color(PLUM);
+
+    let circle = square.below(square);
+    draw.ellipse().xy(circle.xy()).wh(circle.wh()).color(SALMON);
+
+    draw.polygon()
+        .points_colored((0..=360).step_by(45).map(|i| {
+            let rad = deg_to_rad(i as f32 + 22.5);
+            let x = rad.sin() * 150.0;
+            let y = rad.cos() * 150.0;
+            (pt2(x, y), STEELBLUE)
+        }));
+
+    draw.polyline().weight(3.0).points_colored((0..50).map(|i| {
+        let x = i as f32 - 25.0;
+        let point = pt2(x, x.sin()) * 20.0;
+        (point, DEEPPINK)
+    }));
+
+    draw.ellipse().color(INDIGO);
+
+    draw.to_frame(app, &frame).unwrap();
+}
